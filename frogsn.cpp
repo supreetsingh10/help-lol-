2938ee05c633afde4b1973fc0b26311d7f5d0200
Supreet <61558860+supreetsingh10@users.noreply.github.com>
2020-08-12 12:18:24 +0530

    Update frogsn.cpp

--- frogsn.cpp
+++ frogsn.cpp
@@ -1,3 +1,6 @@
+//Trying to apply divide and conquer approach. What I have done is very similar to merge sort. 
+//CODECHEF QUESTION      ***********************      https://www.codechef.com/problems/FROGV      *************************************
+// I am trying to isolate the coordinates of the frog so that and check for the individual next to them and combine the answer. 
 #include<iostream>
 #include<array>
 #include<algorithm>
@@ -13,7 +16,7 @@ int main(){
     for(int i = 0; i<n; i++){
         cin>>frogs[i];
     }
-    sort(frogs, frogs+n);
+    sort(frogs, frogs+n);       // Sorting the frogs on the basis of their value I input in the array frogs[] array. 
     int64_t s = 0;
     last = n-1;
     while(p--){
@@ -32,20 +35,20 @@ int main(){
 bool div(int64_t f[], int64_t first, int64_t last){
     int64_t mid;
     bool flag;
-    if(first<last){
+    if(first<last){                     //Recursive function as long as only first and last are same. 
         mid = first + (last-first)/2;
         div(f, first, mid);
         cout<<"Phase 1: "<<f[first]<<" "<<f[mid]<<endl;
-        div(f, mid+1, last);
+        div(f, mid+1, last);            
         cout<<"Phase 2: "<<f[first]<<" "<<f[mid]<<endl;
-        flag = conq(f, first,mid, mid+1, last);
+        flag = conq(f, first,mid, mid+1, last); //
     } 
     return flag;  
 }
 bool conq(int64_t f[],int64_t fb, int64_t fl, int64_t lb, int64_t ll){
     int64_t na, nb, nc, k; 
     bool flag = true;
-    na = fb;
+    na = fb;                                            // I believe error lies in this code. I don't understand what it is 
     nb = lb; 
     nc = fb; 
     while((na<=fl)&&(nb<=ll)){
@@ -59,8 +62,8 @@ bool conq(int64_t f[],int64_t fb, int64_t fl, int64_t lb, int64_t ll){
 }
 bool checks(int64_t f[], int64_t c1, int64_t c2){
     if((c2-c1)<=k){
-        cout<<f[c2]<<" "<<f[c1]<<endl;
-        return true;
+        cout<<f[c2]<<" "<<f[c1]<<endl;                          
+        return true;                                    // I want to check for individual frogs here. This program is running but it is wrong. 
     }else{
         return false;
     }
